e6f9ed70e185f4c9ae1d672c55a5e24380f03ab8
kanishka jain <kanishkjain784@gmail.com>
2026-03-17 23:54:07 +0530

    Add files via upload

---
+++ comparison2Op.java
@@ -0,0 +1,9 @@
+public class comparison2Op {
+  public static void main(String[] args) {
+    int age = 18;
+
+    System.out.println(age >= 18); // true, old enough to vote
+    System.out.println(age < 18);  // false
+  }
+}
+
