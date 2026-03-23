189f2ce916f790400ebf6cbe3f627a94f7eae148
kanishka jain <kanishkjain784@gmail.com>
2026-03-23 22:47:55 +0530

    Add files via upload

---
+++ concetination3.java
@@ -0,0 +1,10 @@
+public class concetination3 {
+ public static void main(String[] args) {
+    String a = "Java ";
+    String b = "is ";
+    String c = "fun!";
+    String result = a.concat(b).concat(c);
+    System.out.println(result);
+  }
+}
+
