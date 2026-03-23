32aa4350b70fc6c6972b9c8d29f679b1b44de136
kanishka jain <kanishkjain784@gmail.com>
2026-03-23 22:48:28 +0530

    Add files via upload

---
+++ concetination4.java
@@ -0,0 +1,10 @@
+public class concetination4 {
+      public static void main(String[] args) {
+    String firstName = "John ";
+    String lastName = "Doe";
+    System.out.println(firstName.concat(lastName));
+  }
+}
+
+    
+
