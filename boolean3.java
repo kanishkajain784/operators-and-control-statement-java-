12f51a78153772e7155f0fa11f4d3c55aa7841a0
kanishka jain <kanishkjain784@gmail.com>
2026-03-26 23:36:51 +0530

    Add files via upload

---
+++ boolean3.java
@@ -0,0 +1,8 @@
+public class boolean3 {
+  public static void main(String[] args) {
+    int x = 10;
+    int y = 9;
+    System.out.println(x > y); // returns true, because 10 is higher than 9  
+  }
+}
+
