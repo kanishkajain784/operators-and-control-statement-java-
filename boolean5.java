fef987d42188a0caedafbbfb15cc0de626aebb95
kanishka jain <kanishkjain784@gmail.com>
2026-03-26 23:50:17 +0530

    Add files via upload

---
+++ boolean5.java
@@ -0,0 +1,7 @@
+public class boolean5 {
+  public static void main(String[] args) {
+    int x = 10;
+    System.out.println(x == 10); // returns true, because the value of x is equal to 10
+  }
+}
+
