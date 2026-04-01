5abb0145f22941a268b63925e375dff2d90d0551
kanishka jain <kanishkjain784@gmail.com>
2026-04-01 22:36:23 +0530

    Add files via upload

---
+++ elseIf4.java
@@ -0,0 +1,13 @@
+public class elseIf4 {
+  public static void main(String[] args) {
+    int age= 18;
+    
+    if (age < 18) {
+      System.out.println("NOT APPLICABLE TO VOTE");
+    } else if (age < 18) {
+      System.out.println("APPLICABALE FOR VOTE");
+    } else {
+      System.out.println("NOT ELIGIBLE");
+    }
+  }
+}
