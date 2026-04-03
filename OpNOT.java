5930ba231bae2d815338f07055b1ff4b943d9b53
kanishka jain <kanishkjain784@gmail.com>
2026-04-03 23:48:24 +0530

    Add files via upload

---
+++ OpNOT.java
@@ -0,0 +1,12 @@
+public class OpNOT
+{
+  public static void main(String[] args) {
+    int a = 33;
+    int b = 200;
+
+    if (!(a > b)) {
+      System.out.println("a is NOT greater than b");
+    }
+  }
+}
+
