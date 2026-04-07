4bbbcd74255048e6e5fea176823347d3238b693d
kanishka jain <kanishkjain784@gmail.com>
2026-04-07 22:36:31 +0530

    Add files via upload

---
+++ DoWhile2.java
@@ -0,0 +1,12 @@
+public class DoWhile2 {
+
+  public static void main(String[] args) {
+    int i = 10;
+    do {
+      System.out.println("i is " + i);
+      i++;
+    } while (i < 5);
+  }
+}
+    
+
